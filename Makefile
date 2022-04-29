d20915ded33d3501946238bea9b936efc9d353f2
Thomas Burton <tburton5572@gmail.com>
2022-04-29 13:46:01 +0100

    modified lapack include path to work on my desktop

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Change the value of this macro to select your C compiler
 #!!!!!!!!!!!!!!!!#
-CC = gcc-6
+CC = gcc
 #!!!!!!!!!!!!!!!!#
 
 # objects directory
@@ -28,11 +28,11 @@ MAINOBJ = $(patsubst %,$(ODIR)/%,$(_MAINOBJ))
 TESTOBJ = $(patsubst %,$(TESTODIR)/%,$(_TESTOBJ))
 
 # libraries, compile flags and include directories
-#LIBS = -lfftw3 -liniparser -lm -L$(MKLROOT)/lib/intel64 -lmkl_core -lm -lmkl_intel_ilp64 -lmkl_intel_thread -lpthread -lgsl -lgslcblas
-LIBS = -lfftw3 -liniparser -lm -lpthread -llapack -lblas -L/usr/local/opt/openblas/lib
+# LIBS = -lfftw3 -liniparser -lm -L$(MKLROOT)/lib/intel64 -lmkl_core -lm -lmkl_intel_ilp64 -lmkl_intel_thread -lpthread -lgsl -lgslcblas
+LIBS = -lfftw3 -liniparser -lm -lpthread -llapack -llapacke -lblas -L/usr/lib #-L/usr/local/opt/openblas/lib
 
 #-llapacke
-#CFLAGS = -Wall -std=gnu99 -O3 -DMKL_ILP64 -I$(MKLROOT)/include -openmp
+# CFLAGS = -Wall -std=gnu99 -O3 -DMKL_ILP64 -I$(MKLROOT)/include -fopenmp
 CFLAGS = -Wall -std=gnu99 -O3 -fopenmp
 # -profile-functions -profile-loops=all
 # -mkl=sequential
--- src/parameters.c
+++ src/parameters.c
@@ -7,97 +7,97 @@
 #include "parameters.h"
 
 struct Parameters *loadParametersFromParamsFile(void) {
-	/*	Parse arguments from init file */
+    /*  Parse arguments from init file */
 
-	dictionary *dict = iniparser_load("params");
+    dictionary *dict = iniparser_load("params");
 
-	// initialize params struct
-	struct Parameters *params = malloc(sizeof(struct Parameters));
-	
-	// parse parameters	
-	params->Ny        = iniparser_getint(dict, "params:ny", -1);
-	if (params->Ny == -1) {
-		log_err("key Ny was not found in params file");
-		exit(1);
-	}
+    // initialize params struct
+    struct Parameters *params = malloc(sizeof(struct Parameters));
+    
+    // parse parameters 
+    params->Ny        = iniparser_getint(dict, "params:ny", -1);
+    if (params->Ny == -1) {
+        log_err("key Ny was not found in params file");
+        exit(1);
+    }
 
-	params->Nz        = iniparser_getint(dict, "params:nz", -1);
-	if (params->Nz == -1) {
-		log_err("key Nz was not found in params file");
-		exit(1);
-	}
-	
-	params->Re        = iniparser_getdouble(dict, "params:re", -1);
-	if (params->Re == -1) {
-		log_err("key Re was not found in params file");
-		exit(1);
-	}
-	
-	params->Ro     = iniparser_getdouble(dict, "params:ro", -1);
-	if (params->Ro == -1) {
-		log_err("key Ro was not found in params file");
-		exit(1);
-	}
-	
-	params->dt        = iniparser_getdouble(dict, "params:dt", -1);
-	if (params->dt == -1) {
-		log_err("key dt was not found in params file");
-		exit(1);
-	}
+    params->Nz        = iniparser_getint(dict, "params:nz", -1);
+    if (params->Nz == -1) {
+        log_err("key Nz was not found in params file");
+        exit(1);
+    }
+    
+    params->Re        = iniparser_getdouble(dict, "params:re", -1);
+    if (params->Re == -1) {
+        log_err("key Re was not found in params file");
+        exit(1);
+    }
+    
+    params->Ro     = iniparser_getdouble(dict, "params:ro", -1);
+    if (params->Ro == -1) {
+        log_err("key Ro was not found in params file");
+        exit(1);
+    }
+    
+    params->dt        = iniparser_getdouble(dict, "params:dt", -1);
+    if (params->dt == -1) {
+        log_err("key dt was not found in params file");
+        exit(1);
+    }
 
-	params->alpha     = 4*asin(1.0)/iniparser_getdouble(dict, "params:L", -1);
-	params->L         =             iniparser_getdouble(dict, "params:L", -1);
-	if (params->alpha == -1) {
-		log_err("key L was not found in params file");
-		exit(1);
-	}
-	
-	params->T         = iniparser_getdouble(dict, "params:t", -1);
-	if (params->T == -1) {
-		log_err("key T was not found in params file");
-		exit(1);
-	}
-	
-	params->n_it_out  = iniparser_getint(dict, "params:n_it_out", -1);
-	if (params->n_it_out == -1) {
-		log_err("key n_it_out was not found in params file");
-		exit(1);
-	}
+    params->alpha     = 4*asin(1.0)/iniparser_getdouble(dict, "params:L", -1);
+    params->L         =             iniparser_getdouble(dict, "params:L", -1);
+    if (params->alpha == -1) {
+        log_err("key L was not found in params file");
+        exit(1);
+    }
+    
+    params->T         = iniparser_getdouble(dict, "params:t", -1);
+    if (params->T == -1) {
+        log_err("key T was not found in params file");
+        exit(1);
+    }
+    
+    params->n_it_out  = iniparser_getint(dict, "params:n_it_out", -1);
+    if (params->n_it_out == -1) {
+        log_err("key n_it_out was not found in params file");
+        exit(1);
+    }
 
-	params->t_restart = iniparser_getdouble(dict, "params:t_restart", -1);
-	if (params->t_restart == -1) {
-		log_err("key t_restart was not found in params file");
-		exit(1);
-	}
+    params->t_restart = iniparser_getdouble(dict, "params:t_restart", -1);
+    if (params->t_restart == -1) {
+        log_err("key t_restart was not found in params file");
+        exit(1);
+    }
 
-	params->stretch_factor = iniparser_getdouble(dict, "params:stretch_factor", -1);
-	if (params->stretch_factor == -1) {
-		log_err("key stretch_factor was not found in params file");
-		exit(1);
-	}
+    params->stretch_factor = iniparser_getdouble(dict, "params:stretch_factor", -1);
+    if (params->stretch_factor == -1) {
+        log_err("key stretch_factor was not found in params file");
+        exit(1);
+    }
 
-	params->n_threads = iniparser_getint(dict, "params:n_threads", -1);
-	if (params->n_threads == -1) {
-		log_err("key n_threads was not found in params file");
-		exit(1);
-	}
+    params->n_threads = iniparser_getint(dict, "params:n_threads", -1);
+    if (params->n_threads == -1) {
+        log_err("key n_threads was not found in params file"); 
+        exit(1);
+    }
 
-	// params->bctype = iniparser_getint(dict, "params:bctype", -1);
-	// if (params->bctype == -1) {
-	// 	log_err("key bctype was not found in params file. 0 -> zero mass flux, 1 -> zero pg");
-	// 	exit(1);
-	// }
-	// params->A = iniparser_getdouble(dict, "params:A", -1);
-	// if (params->A == -1) {
-	// 	log_err("key A was not found in params file");
-	// 	exit(1);
-	// }
-	// params->eta = iniparser_getdouble(dict, "params:eta", -1);
-	// if (params->eta == -1) {
-	// 	log_err("key eta was not found in params file");
-	// 	exit(1);
-	// }
-	
+    // params->bctype = iniparser_getint(dict, "params:bctype", -1);
+    // if (params->bctype == -1) {
+    //  log_err("key bctype was not found in params file. 0 -> zero mass flux, 1 -> zero pg");
+    //  exit(1);
+    // }
+    // params->A = iniparser_getdouble(dict, "params:A", -1);
+    // if (params->A == -1) {
+    //  log_err("key A was not found in params file");
+    //  exit(1);
+    // }
+    // params->eta = iniparser_getdouble(dict, "params:eta", -1);
+    // if (params->eta == -1) {
+    //  log_err("key eta was not found in params file");
+    //  exit(1);
+    // }
+    
     // allocate space for grid along y
     double *x = malloc(sizeof(double)*params->Ny);
 
@@ -106,22 +106,22 @@ struct Parameters *loadParametersFromParamsFile(void) {
 
     // create grid along y
     for (int i=0; i<params->Ny; i++) {
-    	x[i] = tanh(params->stretch_factor*(i*step-0.5)) / tanh(0.5*params->stretch_factor);
+        x[i] = tanh(params->stretch_factor*(i*step-0.5)) / tanh(0.5*params->stretch_factor);
     }
 
     // allocate space for 
     params->h = malloc(sizeof(double)*(params->Ny-1));
     for (int i=0; i<params->Ny-1; i++) {
-    	params->h[i] = x[i+1] - x[i];
+        params->h[i] = x[i+1] - x[i];
     }
 
     free(x);
-	iniparser_freedict(dict);
-	return params;
+    iniparser_freedict(dict);
+    return params;
 }
 
 void parametersDestroy(struct Parameters *params) {
-	/*	Free up memory of parameters structure */
-	free(params->h);
-	free(params);
+    /*  Free up memory of parameters structure */
+    free(params->h);
+    free(params);
 }
