ff5d5777f924474a36f6d3ee0a2445cc79708162
Thomas Burton <tburton5572@gmail.com>
2022-05-17 13:56:39 +0100

    added command line functionality to video script

--- util/rpcf_util/video.py
+++ util/rpcf_util/video.py
@@ -2,13 +2,14 @@
 
 import os
 import sys
+import argparse
 
 import numpy as np
 
 from rpcf.simulation import SimulationResults
 import pylab
 
-def make_pngs(casedir):
+def make_pngs(casedir, vid_range=None):
     # make sure directories exist
     if not os.path.isdir(os.path.join(os.getcwd(), 'videos/', os.path.basename(casedir[:-1]))):
         os.mkdir(os.path.join(os.getcwd(), 'videos/', os.path.basename(casedir[:-1])))
@@ -16,13 +17,17 @@ def make_pngs(casedir):
     # unpack simulation data
     sim = SimulationResults(casedir)
 
+    # set time range of video
+    if vid_range is None:
+        vid_range = range(len(sim.t))
+
     # define contour levels
     levels = np.linspace(-10, 10, 100)
-    for i in range(len(sim.t)):
+    for i in vid_range:
         snap = sim[sim.t[i]]
         pylab.clf()
-        pylab.contourf(snap.omega.z, snap.omega.y, snap.omega.data, levels, extend="both", cmap=cm.seismic)
-        # pylab.contourf(snap.omega.z, snap.omega.y, snap.omega.data, extend="both", cmap=pylab.cm.seismic)
+        # pylab.contourf(snap.omega.z, snap.omega.y, snap.omega.data, levels, extend="both", cmap=pylab.cm.seismic)
+        pylab.contourf(snap.omega.z, snap.omega.y, snap.omega.data, extend="both", cmap=pylab.cm.seismic)
         pylab.gca().set_aspect(1)
         pylab.xticks([0, 2, 4, 6, 8])
         pylab.yticks([-1, 0, 1])
@@ -31,26 +36,32 @@ def make_pngs(casedir):
         pylab.gca().tick_params(axis='x', direction='out')
         pylab.gca().tick_params(axis='y', direction='out')
         pylab.savefig(os.path.join(os.getcwd(), 'videos/', os.path.basename(casedir[:-1]), 'frame%06d.png' % i), dpi=1000, bbox_inches='tight')
-        print(i), len(sim.t)
+        print(i)
 
-def pngs2mp4(casedir, video_name, frame_rate=60):
+def pngs2mp4(casedir, video_name, frame_rate):
     video_dir = os.path.join(os.getcwd(), 'videos/', os.path.basename(casedir[:-1]))
     ffmpeg_call = f"ffmpeg -r {frame_rate} -pattern_type glob -i '{video_dir}/frame*.png' -vcodec libx264 {video_dir}/{video_name}"
     os.system(ffmpeg_call)
 
 if __name__ == '__main__':
     # parse command line arguments
-    if len(sys.argv) == 1:
-        print('Invalid arguments given! Missing simulation directory path!')
-        sys.exit(1)
-    casedir = sys.argv[1]
-    try:
-        frame_rate = sys.argv[2]
-    except IndexError:
-        frame_rate = 60
+    parser = argparse.ArgumentParser()
+    parser.add_argument('casedir', type=str)
+    parser.add_argument('--frame_rate', '-r', type=int)
+    parser.add_argument('--vid_range', '-v', type=str)
+    parser.add_argument('--vid_name', '-n', type=str)
+    parser.add_argument('--no_vid', action='store_true')
+    args = parser.parse_args()
+    if args.frame_rate is None:
+        args.frame_rate = 60
+    if args.vid_range is not None:
+        args.vid_range = range(*list(map(int, args.vid_range.split('-'))))
+    if args.vid_name is None:
+        args.vid_name = 'wearenumberone.mp4'
 
     # generate pngs
-    make_pngs(casedir)
+    make_pngs(args.casedir, vid_range=args.vid_range)
 
     # make video using ffmpeg
-    pngs2mp4(casedir, 'wearenumberone.mp4', frame_rate=frame_rate)
+    if not args.no_vid:
+        pngs2mp4(args.casedir, args.vid_name, frame_rate=args.frame_rate)
